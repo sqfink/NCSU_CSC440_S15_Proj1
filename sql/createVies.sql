70b9fbe872fd19f2b9d6afedf339e3484a0b3a01
sqfink <sqfink@ncsu.edu>
2015-04-16 11:09:16 -0400

    Remove unused file

--- sql/createVies.sql
+++
@@ -1,6 +0,0 @@
-CREATE VIEW v_generalopenparking AS SELECT * FROM `parkingspots` WHERE `lotnumber` IN  (
-	SELECT `lotnumber` FROM `parkinglots` WHERE `lotnumber` NOT IN (
-		SELECT `lotnumber` FROM `parkinglotsnear`
-	)
-) AND `snumber` IS NULL;
-
